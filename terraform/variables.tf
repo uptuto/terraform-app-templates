1b12eb071019ff7f7397acb675d853427e55c171
himajak333 <himajak333@gmail.com>
2025-06-15 19:34:46 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
 variable "app_name" { type = string }
 variable "additional_envs" { type = list(string) }
 variable "prd_protection" { type = bool }
+variable "github_token" {
+  type        = string
+  description = "GitHub Personal Access Token"
+  sensitive   = true
+}
