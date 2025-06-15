953ab17c00908c3e3091b21973f3dcdea662c435
himajak333 <himajak333@gmail.com>
2025-06-15 00:51:36 -0400

    Update main.tf

--- terraform/modules/github_app/main.tf
+++ terraform/modules/github_app/main.tf
@@ -38,18 +38,15 @@ resource "github_team_repository" "permissions" {
 }
 
 resource "github_branch_protection_v3" "main" {
-  count         = var.prd_protection ? 1 : 0
-  repository    = github_repository.repo.name
-  pattern       = "main"
+  repository = github_repository.repo.name
+  branch     = "main"
 
   required_pull_request_reviews {
     required_approving_review_count = 2
   }
+
+  restrictions {
+    teams = [github_team.teams["approver"].slug]
+  }
 }
 
-resource "github_branch_protection_v3_restrictions" "main" {
-  count      = var.prd_protection ? 1 : 0
-  repository = github_repository.repo.name
-  branch     = github_branch_protection_v3.main[0].pattern
-  teams      = [github_team.teams["approver"].slug]
-}
